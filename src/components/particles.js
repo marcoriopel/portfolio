2dcc116c0958e65879867bb9519a2bb7d3286ede
marco <marco.riopel@hotmail.com>
2020-11-30 12:10:28 -0500

    Implement particles for background

---
+++ src/components/particles.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+
+export default function ParticlesContainer() {
+    return (
+        <Particles
+            style={{ position: 'absolute' }}
+            params={{
+                particles: {
+                    number: {
+                        value: 75,
+                    },
+                    size: {
+                        value: 3,
+                    },
+                },
+                interactivity: {
+                    events: {
+                        onhover: {
+                            enable: true,
+                            mode: 'repulse',
+                        },
+                    },
+                },
+            }}
+        />
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Header from '../components/header';
 import Container from '../components/container';
+import styles from './index.module.css';
+import ParticlesContainer from '../components/particles';
 
 export default function Home() {
     return (
-        <Container>
-            <Header headerText="Hi, I'm Marco" />
-            <p style={{ color: `white`, fontSize: `40px` }}>Welcome to my website</p>
-            <h3 style={{ color: `white`, fontSize: `72px`, paddingTop: '1200px' }}>Welcome to my website</h3>
-        </Container>
+        <div style={{ position: 'relative' }}>
+            <ParticlesContainer />
+            <Container>
+                <Header headerText="Hi, I'm Marco" />
+                <p style={{ color: `white`, fontSize: `35px` }}>Welcome to my website</p>
+                <div className={(styles.shapes, styles.float)}>
+                    <div className={(styles.sqare, styles.spin)}></div>
+                    <div className={(styles.circle, styles.spin)}></div>
+                </div>
+
+                <h3 style={{ color: `white`, fontSize: `72px`, paddingTop: '1200px' }}>Welcome to my website</h3>
+            </Container>
+        </div>
     );
 }
