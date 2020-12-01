d8dc4ff7c7433b4ba9c73a9b57702a475aedb351
marco <marco.riopel@hotmail.com>
2020-12-01 15:15:49 -0500

    Fix favicon with new icon

--- src/components/head.js
+++ src/components/head.js
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet';
 export default function Head() {
     return (
         <Helmet title="Marco's Portfolio">
-            <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+            <link rel="icon" type="image/png" href="favicon.ico" sizes="16x16" />
         </Helmet>
     );
 }
