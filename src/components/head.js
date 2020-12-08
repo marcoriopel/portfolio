df7f7e5bcee27f225ada9e1cb077af5d7b63088b
marco <marco.riopel@hotmail.com>
2020-12-08 16:01:41 -0500

    Auto scale for mobile devices

--- src/components/head.js
+++ src/components/head.js
@@ -5,6 +5,7 @@ export default function Head() {
     return (
         <Helmet title="Marco's Portfolio">
             <link rel="icon" type="image/png" href="favicon.ico" sizes="16x16" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0,  minimum-scale=1.0"></meta>
         </Helmet>
     );
 }
